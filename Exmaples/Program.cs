15f055b680521804a9b61ac671e4b72df0aeb64c
nix <code.03bb@gmail.com>
2025-07-08 19:08:49 +0200

    update examples

--- Exmaples/Program.cs
+++ Exmaples/Program.cs
@@ -2,20 +2,52 @@
 
 using NxRelay;
 
-Events events = new();
-int counter = 0;
-IAsyncDisposable dispose = events.Subscribe<int>(
-    i =>
-    {
-        counter = Interlocked.Increment(ref counter);
-    });
+namespace Exmaples;
 
-IEnumerable<Task<bool>> tasks = new List<Task<bool>>(10_000);
-for (int i = 0; i < 10_000; i++)
+internal static class Program
 {
-    tasks = tasks.Append(events.Publish(i));
-}
+    public static async Task<int> Main(string[] args)
+    {
+        Events events = new();
+        int counter = 0;
+        IAsyncDisposable dispose = events.Subscribe<int>(
+            i =>
+            {
+                counter = Interlocked.Increment(ref counter);
+            });
+
+        IEnumerable<Task<bool>> tasks = new List<Task<bool>>(10_000);
+        for (int i = 0; i < 10_000; i++)
+        {
+            tasks = tasks.Append(events.Publish(i));
+        }
         
-await Task.WhenAll(tasks);
-await dispose.DisposeAsync();
-Console.WriteLine($"Counter reached: {counter}");
+        await Task.WhenAll(tasks);
+        await dispose.DisposeAsync();
+        Console.WriteLine($"Counter reached: {counter}");
+
+        Mediator mediator = new();
+        mediator.Register(new MyRequestHandler());
+        Response response = await mediator.SendAsync<Request, Response>(new Request("Hello, World!"));
+        Console.WriteLine($"Response: {response.Content}");
+        return 0;
+    }
+}
+
+public readonly struct Request(string content) : IRequest<Response>
+{
+    public string Content { get; } = content;
+}
+
+public readonly struct Response(string content) 
+{
+    public string Content { get; } = content;
+}
+public class MyRequestHandler : IRequestHandler<Request, Response>  
+{
+    public ValueTask<Response> HandleAsync(Request request, CancellationToken ct)
+    {
+        // Simulate some processing
+        return new ValueTask<Response>(new Response($"Processed: {request.Content}"));
+    }
+}
