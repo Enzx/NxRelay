68291cfc00ce1dd451b14799c0a43038606820cd
Mohamad Iraji <code.03bb@gmail.com>
2025-07-09 01:05:02 +0200

    Add assembly scanning support

--- NxRelay/MessagingServiceCollectionExtensions.cs
+++ NxRelay/MessagingServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace NxRelay;
 
@@ -12,24 +13,41 @@ public static class MessagingServiceCollectionExtensions
     /// <summary>
     /// Registers the Events aggregator, Mediator and any discovered handlers in the DI container.
     /// </summary>
-    public static IServiceCollection AddMessaging(this IServiceCollection services)
+    public static IServiceCollection AddMessaging(this IServiceCollection services, params Assembly[] assemblies)
     {
         services.AddScoped<Events>();
         services.AddScoped<IMediator, Mediator>();
 
-        // open-generic registrations for handlers
-        services.Scan(scan => scan
-            .FromApplicationDependencies()
-            .AddClasses(c => c.AssignableTo(typeof(IHandler<>)))
-            .AsImplementedInterfaces()
-            .WithScopedLifetime());
+        if (assemblies is { Length: > 0 })
+        {
+            // open-generic registrations for handlers from provided assemblies
+            services.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(c => c.AssignableTo(typeof(IHandler<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
 
-        services.Scan(scan => scan
-            .FromApplicationDependencies()
-            .AddClasses(c => c.AssignableTo(typeof(IRequestHandler<,>)))
-            .AsImplementedInterfaces()
-            .WithScopedLifetime());
+            services.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(c => c.AssignableTo(typeof(IRequestHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+        }
+        else
+        {
+            // fallback to scanning application dependencies
+            services.Scan(scan => scan
+                .FromApplicationDependencies()
+                .AddClasses(c => c.AssignableTo(typeof(IHandler<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+            services.Scan(scan => scan
+                .FromApplicationDependencies()
+                .AddClasses(c => c.AssignableTo(typeof(IRequestHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+        }
 
         return services;
-    }
-}
+    }}
